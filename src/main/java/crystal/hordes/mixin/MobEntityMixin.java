91d8b542055d9df4940f1d347b2853941c344cb3
crystalx0375 <crystalx0375@gmail.com>
2026-07-18 19:50:14 +0300

    fix: Fixed pull request...

--- src/main/java/crystal/hordes/mixin/MobEntityMixin.java
+++ src/main/java/crystal/hordes/mixin/MobEntityMixin.java
@@ -68,7 +68,7 @@ public abstract class MobEntityMixin extends LivingEntity implements IHordes {
                 }
             }
 
-            if (HordesConfig.get().ONLY_TARGET_PLAYERS && !(target instanceof PlayerEntity)) {
+            if (HordesConfig.ONLY_TARGET_PLAYERS && !(target instanceof PlayerEntity)) {
                 ci.cancel();
             }
         }
@@ -98,7 +98,7 @@ public abstract class MobEntityMixin extends LivingEntity implements IHordes {
     }
 
     @Unique private void setTargetSelector(MobEntity host) {
-        if (!HordesConfig.get().ONLY_TARGET_PLAYERS) {
+        if (!HordesConfig.ONLY_TARGET_PLAYERS) {
             this.targetSelector.add(2, new ActiveTargetGoal<>(host, MobEntity.class, 10, false, true,
                     entity -> {
                         if (entity == host || !entity.isAlive()) return false;
